dbde11e5355a0a54ac3b8aacdabe9b2bbf12d412
mutsuki <mutsuki@momo86.net>
2022-08-25 14:52:51 +0900

    Add tests for double2 and float2

--- test/main.cu
+++ test/main.cu
@@ -3,15 +3,23 @@
 #include <cu_exp_statistics.hpp>
 
 template <class T>
+struct base_t {
+	using type = T;
+	static const unsigned num_elements = 1;
+};
+template <> struct base_t<double2> {using type = double;static const unsigned num_elements = 2;};
+template <> struct base_t<float2 > {using type = float; static const unsigned num_elements = 2;};
+
+template <class T>
 void test(const std::size_t m, const std::size_t n) {
-	const auto size = m * n;
+	const auto size = m * n * base_t<T>::num_elements;
 	std::mt19937 mt(std::random_device{}());
-	std::uniform_real_distribution<T> dist(-10, 10);
+	std::uniform_real_distribution<typename base_t<T>::type> dist(-10, 10);
 
 	T* test_matrix_ptr;
 	cudaMallocManaged(&test_matrix_ptr, sizeof(T) * size);
 	for (std::size_t i = 0; i < size; i++) {
-		test_matrix_ptr[i] = dist(mt);
+		reinterpret_cast<typename base_t<T>::type*>(test_matrix_ptr)[i] = dist(mt);
 	}
 	cudaDeviceSynchronize();
 
@@ -27,9 +35,14 @@ void test(const std::size_t m, const std::size_t n) {
 		}
 	}
 
+	std::printf("JSON: %s\n", mtk::cu_exp_statistics::to_json(result).c_str());
+
 	cudaFree(test_matrix_ptr);
 }
 
 int main() {
-	test<float>(10000, 10000);
+	test<float  >(20000, 20000);
+	test<double >(20000, 20000);
+	test<float2 >(20000, 20000);
+	test<double2>(20000, 20000);
 }
